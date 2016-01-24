11a0496245fab28b4d50a66cf52d43963ec260b2
Brandon Pearce <kingbdogz_war3freak@hotmail.com>
2016-01-25 00:59:23 +0800

    Added some tilesets, added player, game manager, physics

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	private World world;
+
+	// Use this for initialization
+	void Awake () {
+		this.world = GetComponent<World> ();
+		InitGame();
+	}
+
+	void InitGame()
+	{
+		this.world.Generate ();
+	}	
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Script/World.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class World : MonoBehaviour {
+
+	public GameObject tileForFloor;
+
+	private Transform tileHolder;
+
+	private List<Vector3> gridPositions = new List<Vector3>();
+
+	private int width = 20, height = 2;
+
+	void SetupTiles()
+	{
+		tileHolder = new GameObject ("Tiles").transform;
+
+		for (float x = -10; x < this.width; x += 0.5f)
+		{
+			for (float y = -30; y < -2; y += 0.5f)
+			{
+				GameObject tileInstance = Instantiate(this.tileForFloor, new Vector3(x, y, 0.0F), Quaternion.identity) as GameObject;
+
+				tileInstance.transform.SetParent(this.tileHolder);
+			}
+		}
+	}
+
+	public void Generate()
+	{
+		this.SetupTiles ();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
